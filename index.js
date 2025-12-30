1ce0aaf681563282430788a243d5adf1abe34acb
Naveenmallada9090 <malladanaveen@gmail.com>
2025-12-30 23:25:00 +0530

    step 5 , added admin router

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { UserRoutes } = require("./routes/user");
 const { CourseRoutes } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 const app = express();
 
 app.use("/api/v1/user", UserRoutes );
+app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", CourseRoutes);
 
-app.listen(3000); 
+app.listen(3000);  
